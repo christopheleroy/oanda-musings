def18c88e4702239af5d820a7be20683c58a0c6b
christopheleroy <tof@cleroy.net>
2017-07-31 14:01:16 -0400

    support refreshing the dataset of the current month if it has been partially extracted/cached

--- drafts/extractor.py
+++ drafts/extractor.py
@@ -13,7 +13,7 @@ parser.add_argument('--select', nargs='?',
                     help="valid currency-pair")
 parser.add_argument('--dir', nargs='?', help='target directory')
 parser.add_argument('--slice', nargs='?', default="M1")
-
+parser.add_argument('--quick', action='store_true', help='to quickly extend a cache for what is missing in the month')
 parser.add_argument('--year', nargs='?', type=int, default = None)
 parser.add_argument('--month', nargs='?', type=int, default = None)
 parser.add_argument('--instruments', action='store_true')
@@ -42,12 +42,14 @@ def collectInstruments(dir):
         json.dump(stuff, jsonf)
 
 
-def collectForMonth(pair, year, month, dir, granularity="S5"):
+def collectForMonth(pair, year, month, dir, granularity="S5", refresh=True):
     kwargs = {"granularity":granularity, "price":"MBA"}
 
     batches = ( ("00:00:00", "03:59:59"), ("04:00:00", "07:59:59"),
                 ("08:00:00", "11:59:59"), ("12:00:00", "15:59:59"),
                 ("16:00:00", "19:59:59"), ("20:00:00", "23:59:59"))
+
+    precache = []
 
     daysOfMonth = (00,31,28,31,30,31,30,31,31,30,31,30,31)
     days = daysOfMonth[month]
@@ -57,8 +59,20 @@ def collectForMonth(pair, year, month, dir, granularity="S5"):
         if(year % 4 == 0 and (not day % 100 ==0 or day % 400 ==0)):
             days+=1
 
+    if(not refresh):
+        from candlecache import SliceRowIterator
+        _since = "{}-{}-{}T00:00:00.000000000Z".format(year, month, "01")
+        _till  = "{}-{}-{}T23:59:59:999999999Z".format(year, month, days)
+        precache = list( SliceRowIterator(dir,pair,granularity, _since, _till, api) )
+
     allCandles = []
     timeToBreak = False
+
+    if(len(precache)>0 and not refresh):
+        for c in precache:
+            row = [c.time, c.bid.o, c.ask.o, c.mid.o, c.bid.l, c.ask.l, c.mid.l, c.bid.h, c.ask.h, c.mid.h, c.bid.c, c.ask.c, c.mid.c, c.volume ]
+            allCandles.append(row)
+
     header = ["time", "bid-o", "ask-o", "mid-o", "bid-l", "ask-l", "mid-l", "bid-h", "ask-h", "mid-h", "bid-c", "ask-c", "mid-c", "volume"]
     for day in range(days):
         d = str(day+1)
@@ -70,6 +84,11 @@ def collectForMonth(pair, year, month, dir, granularity="S5"):
             toTs   = "{}T{}.000000000Z".format(dstamp, b[1])
             kwargs["fromTime"]=fromTs
             kwargs["toTime"]=toTs
+            if(not refresh):
+                if(len(allCandles)>0 and cmp(toTs, allCandles[-1][0])<0):
+                    # no need to refresh what is way in the past...
+                    continue
+
             print kwargs
             resp = api.instrument.candles(pair, **kwargs)
             # import pdb; pdb.set_trace()
@@ -77,7 +96,7 @@ def collectForMonth(pair, year, month, dir, granularity="S5"):
 
                 if(resp.body.has_key("errorMessage") and resp.body["errorMessage"] == "Invalid value specified for 'to'. Time is in the future"):
                     del kwargs['toTime']
-                    print kwargs
+                    print ('future is not accepted', kwargs)
                     resp = api.instrument.candles(pair, **kwargs)
                     timeToBreak = True
                 else:
@@ -86,7 +105,7 @@ def collectForMonth(pair, year, month, dir, granularity="S5"):
             candles = resp.get('candles',200)
             candles.sort(lambda a,b: cmp(a.time, b.time))
             for c in candles:
-                if(len(allCandles)>1 and cmp(c.time, allCandles[-1][0])<0): continue
+                if(len(allCandles)>0 and cmp(c.time, allCandles[-1][0])<0): continue
                 row = [c.time, c.bid.o, c.ask.o, c.mid.o, c.bid.l, c.ask.l, c.mid.l, c.bid.h, c.ask.h, c.mid.h, c.bid.c, c.ask.c, c.mid.c, c.volume ]
                 allCandles.append(row)
             if(timeToBreak): break
@@ -100,6 +119,6 @@ def collectForMonth(pair, year, month, dir, granularity="S5"):
 
 
 if(not args.instruments):
-    collectForMonth(args.select, args.year, args.month, args.dir, args.slice)
+    collectForMonth(args.select, args.year, args.month, args.dir, args.slice, not args.quick)
 else:
     collectInstruments(args.dir)
